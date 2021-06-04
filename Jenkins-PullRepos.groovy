7674e3d3acca03c4f54d88377c0074f976a7a83d
JOHN OSHIKOYA <72477831+john24rel@users.noreply.github.com>
2021-06-04 01:38:17 -0500

    Update Jenkins-PullRepos.groovy

--- Jenkins-PullRepos.groovy
+++ Jenkins-PullRepos.groovy
@@ -5,11 +5,10 @@ node("ansible"){
            withCredentials([usernamePassword(credentialsId: 'git-access', passwordVariable: 'GIT_TOKEN', usernameVariable: 'GIT_USERNAME' )]) {
             stage("john24rel repo"){
              dir("/home/jenkins/john24rel/checked"){
-              sh """
-              set +x
-              mkdir -p /home/jenkins/git
-              git clone https://john24rel:${GIT_TOKEN}@github.com/john24rel/checked.git      
-              """
+              sh ```
+             set +x
+             git clone https://john24rel:${GIT_TOKEN}@github.com/john24rel/checked.git    
+              ```
           stage("Pull Repo"){
            git credentialsId: 'git', url: 'https://github.com/john24rel/pull-all-repo.git'
 
