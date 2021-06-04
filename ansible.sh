b20b4f30b90c739f6b80e92526f0e3f8d80f62fe
JOHN OSHIKOYA <72477831+john24rel@users.noreply.github.com>
2021-06-03 22:05:37 -0500

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,5 @@
-ansible localhost -m ping
-ansible-playbook -i localhost  pull.yml -b
 export ANSIBLE_FORCE_COLOR=true
+ansible localhost -m ping
+export ANSIBLE_FORCE_COLOR=true
+sudo -E ansible-playbook -i localhost  pull.yml -b
+
