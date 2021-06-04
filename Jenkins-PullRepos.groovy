5058939f9ef7f64431ea35059038fa0e3cccc9a5
JOHN OSHIKOYA <72477831+john24rel@users.noreply.github.com>
2021-06-04 00:20:58 -0500

    Update Jenkins-PullRepos.groovy

--- Jenkins-PullRepos.groovy
+++ Jenkins-PullRepos.groovy
@@ -1,27 +1,27 @@
 node("ansible"){  
-      withCredentials([usernamePassword(credentialsId: 'git-access', passwordVariable: 'GIT_TOKEN', usernameVariable: 'GIT_USERNAME')]) {
-      stage("john24rel repo"){
-      sh """
-        mkdir -p /home/jenkins/john24rel /home/jenkins/git
-        """
-      dir("/home/jenkins/john2rel")
-      git credentialsId: 'git', url: 'https://github.com/john24rel/checked.git'
-    stage("Pull Repo"){
-     git credentialsId: 'git', url: 'https://github.com/john24rel/pull-all-repo.git'
-     
-     stage("script run"){
-     sh """
-          git config --global user.email "john@yahoo.com"
-          git config --global user.name "John Oshikoya"
-         . ansible.sh 
-        """
-     ansiColor('xterm') {
-     ansiblePlaybook colorized: true, installation: 'ansible2.5.11', inventory: 'localhost', playbook: 'pull.yml'
+            withCredentials([usernamePassword(credentialsId: 'git-access', passwordVariable: 'GIT_TOKEN', usernameVariable: 'GIT_USERNAME')]) {
+            stage("john24rel repo"){
+            sh """
+              mkdir -p /home/jenkins/john24rel /home/jenkins/git
+              """
+                  dir("/home/jenkins/john2rel"){
+            git credentialsId: 'git', url: 'https://github.com/john24rel/checked.git'
+          stage("Pull Repo"){
+           git credentialsId: 'git', url: 'https://github.com/john24rel/pull-all-repo.git'
+
+           stage("script run"){
+                 sh """
+                      git config --global user.email "john@yahoo.com"
+                      git config --global user.name "John Oshikoya"
+                     . ansible.sh 
+                    """
+                 ansiColor('xterm') {
+                 ansiblePlaybook colorized: true, installation: 'ansible2.5.11', inventory: 'localhost', playbook: 'pull.yml'
+            }
           }
         }
       }
     }
   }
 }
- 
      
